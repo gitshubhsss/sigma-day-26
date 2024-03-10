44aad643540d6cf74c6361f46d2773590ff6c5f5
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-10 20:56:52 +0530

    strings in js

---
+++ stringsInJavaScript.js
@@ -0,0 +1,14 @@
+let myName="shubham ranjane";
+myName[1];
+console.log(myName[myName.length-1]);
+console.log(myName[1]);
+console.log(myName[2]);
+console.log(myName[3]);
+let size=myName.length;
+console.log(size);
+
+let firstName="shubham";
+let lastName="ranjane";
+
+let fullName = firstName+" "+lastName;
+console.log(fullName);
