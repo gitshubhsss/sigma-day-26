da28ac41cd593d6a11f41f23a0e805b143fdd918
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-10 20:36:27 +0530

    concept of preincrement and post increment

---
+++ uniryOperator.js
@@ -0,0 +1,3 @@
+let num=5;//num=5
+let newNum=num++; //newNum=5 num=6 first it taking the variable and then changing the value
+newNum=++num;//num=7 and newNum=7 first it will change the value and then it will assign it to the perticular variable
