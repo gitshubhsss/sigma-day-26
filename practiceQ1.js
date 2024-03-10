2d0d5689940e4d887fb6cd6f4b9b626b10a46516
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-10 21:05:17 +0530

    practice 1

---
+++ practiceQ1.js
@@ -0,0 +1,14 @@
+//declare your name as a string and prints its length in js
+let myName="shubham ranjane";
+let len=myName.length;
+console.log(len);
+
+//declare your name as a string and print its first character and then last character
+
+let fisrtName="shubham";
+console.log(fisrtName[0]);//this is first character
+console.log(fisrtName[fisrtName.length-1]);//it is the last character
+
+let emptyString=" "
+len=emptyString.length;
+console.log(len);
